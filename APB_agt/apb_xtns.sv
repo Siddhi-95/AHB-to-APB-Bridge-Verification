7fca0b56e0d8ebba4f94c973df825749ec90a368
Siddhi <100703756+Siddhi-95@users.noreply.github.com>
2023-07-02 20:47:03 +0530

    Update apb_xtns.sv

--- APB_agt/apb_xtns.sv
+++ APB_agt/apb_xtns.sv
@@ -1,6 +1,6 @@
-class apb_xtns extends uvm_sequence_item;
+class apb_xtn extends uvm_sequence_item;
 
-        `uvm_object_utils(apb_xtns)
+        `uvm_object_utils(apb_xtn)
 
         logic Penable, Pwrite; //APB strobe, APB transfer direction - master
 
@@ -14,18 +14,22 @@ class apb_xtns extends uvm_sequence_item;
         logic [31:0] Paddr;
 
         //APB select - master
-        logic Pselx;
+        logic [3:0] Pselx;
+
+	constraint Pselx_count { Pselx dist { 4'b0001:=4, 4'b0010:=4, 4'b0100:=4, 4'b1000:=4  } ;}
 
         extern function void do_print(uvm_printer printer);
 endclass
 
-function void apb_xtns::do_print(uvm_printer printer);
+function void apb_xtn::do_print(uvm_printer printer);
 
         super.do_print(printer);
         printer.print_field("Paddr", this.Paddr, 32, UVM_HEX);
         printer.print_field("Penable", this.Penable, 1, UVM_DEC);
         printer.print_field("Pwrite", this.Pwrite, 1, UVM_DEC);
-        printer.print_field("Pselx", this.Pselx, 1, UVM_DEC);
+        printer.print_field("Pselx", this.Pselx, 4, UVM_DEC);
         printer.print_field("Prdata", this.Prdata, 32, UVM_HEX);
+	printer.print_field("Pwdata", this.Pwdata, 32, UVM_HEX);
 
 endfunction
+
