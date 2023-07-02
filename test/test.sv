baa6312e8d272ce697003a40888e6485cdd029cd
Siddhi <100703756+Siddhi-95@users.noreply.github.com>
2023-07-02 21:02:49 +0530

    Update test.sv

--- test/test.sv
+++ test/test.sv
@@ -1,123 +1,161 @@
-class base_test extends uvm_test;
+class ahb_apb_bridge_test extends uvm_test;
 
-        `uvm_component_utils(base_test)
+	`uvm_component_utils(ahb_apb_bridge_test)
 
-        AHB_APB_env e_cfg;
-        AHB_APB_env_config m_cfg;
+	ahb_apb_env_config m_cfg;
+	ahb_apb_env e_cfg;
 
-        ahb_config ahbcfg[];
-        apb_config apbcfg[];
+	ahb_agent_config ahb_cfg[];
+	apb_agent_config apb_cfg[];
 
-        bit has_ahb_agent = 1;
-        bit has_apb_agent = 1;
-        int no_of_ahb_agents = 1;
-        int no_of_apb_agents = 1;
+	int no_of_ahb_agents=1;
+	int no_of_apb_agents=1;
+	bit has_scoreboard=1;
+	bit has_virtual_sequencer=1;
 
-        extern function new(string name = "base_test", uvm_component parent);
-        extern function void build_phase(uvm_phase phase);
-endclass: base_test
+	extern function new(string name="TEST",uvm_component parent);
+	extern function void build_phase(uvm_phase phase);
+	//extern task run_phase(uvm_phase);
 
-///////-Constructor class-------///
-function base_test::new(string name = "base_test", uvm_component parent);
-        super.new(name, parent);
-endfunction: new
+endclass
 
-///////--Build phase--------/////
-function void base_test:: build_phase(uvm_phase phase);
-        super.build_phase(phase);
+//----------Constructor-----------//
+function ahb_apb_bridge_test:: new(string name="TEST",uvm_component parent);
+	super.new(name,parent);
+endfunction
 
-        m_cfg = AHB_APB_env_config::type_id::create("AHB_APB_env_config", this);
-        e_cfg = AHB_APB_env::type_id::create("AHB_APB_env", this);
+//-----------Build Phase----------//
+function void ahb_apb_bridge_test:: build_phase(uvm_phase phase);
+	super.build_phase(phase);	
 
-        if(has_ahb_agent)
-                m_cfg.ahb_cfg = new[m_cfg.no_of_ahb_agents];
+	m_cfg = ahb_apb_env_config::type_id::create("ahb_apb_env_config", this);
+        e_cfg = ahb_apb_env::type_id::create("ahb_apb_env", this);
+	//these shud be written before declaring dynamic array
 
-        if(has_apb_agent)
-				m_cfg.ahb_cfg = new[m_cfg.no_of_ahb_agents];
+	if(m_cfg.has_ahb_agent)
+	begin
+		m_cfg.ahb_cfg=new[no_of_ahb_agents];
+	end
+	
+	if(m_cfg.has_apb_agent)
+	begin
+		m_cfg.apb_cfg=new[no_of_apb_agents];
+	end
+	
+	//m_cfg = ahb_apb_env_config::type_id::create("ahb_apb_env_config", this);
+	//e_cfg = ahb_apb_env::type_id::create("ahb_apb_env", this);
 
-        if(has_apb_agent)
-                m_cfg.apb_cfg = new[m_cfg.no_of_apb_agents];
+	ahb_cfg=new[no_of_ahb_agents];
+	apb_cfg=new[no_of_apb_agents];
+	
+	foreach(apb_cfg[i])
+	begin
+		apb_cfg[i]=apb_agent_config::type_id::create($sformatf("apb_cfg[%0d]",i));
+		if(!uvm_config_db #(virtual apb_if)::get(this,"","apb_vif",apb_cfg[i].vif))
+			`uvm_fatal("TEST","cannot get config data");
 
-    //AHB agent configuration:
-    if(has_ahb_agent)
-        begin
-            //ahbcfg = new[m_cfg.no_of_ahb_agents];
-           ahbcfg = new[m_cfg.no_of_ahb_agents];
+		apb_cfg[i].is_active=UVM_ACTIVE;
+		m_cfg.apb_cfg[i]=apb_cfg[i];
 
-            foreach(ahbcfg[i])
-                begin
-                    ahbcfg[i]=ahb_config::type_id::create($sformatf("ahbcfg[%0d]",i));
+		//uvm_config_db #(apb_agent_config)::set(this,$sformatf("*agt[%0d]*",i),"apb_agent_config",m_cfg.apb_cfg[i]);
 
-                    //get the virtual interface from the config class:
-                    if(!uvm_config_db #(virtual ahb_if)::get(this,"","vif_ahb", ahbcfg[i].aif))
-                                `uvm_fatal("VIF CONFIG-WRITE","Cannot get() vif from uvm_config_db. Have you set it?")
 
-                        ahbcfg[i].is_active = UVM_ACTIVE;
-                        m_cfg.ahb_cfg[i]=ahbcfg[i];
-                end
-        end
+	 
+	end
 
-    //APB agent configuration:
-    if(has_apb_agent)
-        begin
-            apbcfg = new[m_cfg.no_of_apb_agents];
-            foreach(apbcfg[i])
-                begin
-                    apbcfg[i]=apb_config::type_id::create($sformatf("apbcfg[%0d]",i));
-					//get the virtual interface from the config class:
-                    if(!uvm_config_db #(virtual apb_if)::get(this,"","vif_apb", apbcfg[i].aif))
-                                `uvm_fatal("VIF CONFIG-WRITE","Cannot get() vif from uvm_config_db. Have you set it?")
+	//uvm_config_db #(apb_agent_config)::set(this,$sformatf("*agt[%0d]*",i),"apb_agent_config",m_cfg.apb_cfg[i]);
 
-                        apbcfg[i].is_active = UVM_ACTIVE;
-                        m_cfg.apb_cfg[i]=apbcfg[i];
-                end
-        end
+	foreach(ahb_cfg[i])
+	begin
+		ahb_cfg[i]=ahb_agent_config::type_id::create($sformatf("ahb_cfg[%0d]",i));
+		if(!uvm_config_db #(virtual ahb_if)::get(this,"","ahb_vif",ahb_cfg[i].vif))
+			`uvm_fatal("TEST","cannot get config data");
 
-        m_cfg.has_ahb_agent = has_ahb_agent;
-        m_cfg.has_apb_agent = has_apb_agent;
-        m_cfg.no_of_ahb_agents = no_of_ahb_agents;
-        m_cfg.no_of_apb_agents = no_of_apb_agents;
+		ahb_cfg[i].is_active=UVM_ACTIVE;
+		m_cfg.ahb_cfg[i]=ahb_cfg[i];
+	
+		//uvm_config_db #(ahb_agent_config)::set(this,$sformatf("*agt[%0d]*",i),"ahb_agent_config",m_cfg.ahb_cfg[i]);
 
-        uvm_config_db #(AHB_APB_env_config)::set(this, "*", "AHB_APB_env_config", m_cfg);
 
-        //e_cfg = AHB_APB_env::type_id::create("AHB_APB_env", this);
-endfunction: build_phase
+	end
+
+	//uvm_config_db #(ahb_agent_config)::set(this,$sformatf("*agt[%0d]*",i),"ahb_agent_config",m_cfg.ahb_cfg[i]);
+
+
+	m_cfg.no_of_ahb_agents=no_of_ahb_agents;
+	m_cfg.no_of_apb_agents=no_of_apb_agents;
+	m_cfg.has_scoreboard=has_scoreboard;
+	m_cfg.has_virtual_sequencer=has_virtual_sequencer;
+	
+	uvm_config_db#(ahb_apb_env_config) ::set(this,"*","ahb_apb_env_config",m_cfg);
+endfunction
 
 
 ////---------test 1------//////
-class test_1 extends base_test;
+class test_1 extends ahb_apb_bridge_test;
 
         `uvm_component_utils(test_1)
 
-        vseq t_seqh;
+       // vseq t_seqh;
+	test_sequence t_seqh;
 
         extern function new(string name = "test_1", uvm_component parent);
         extern function void build_phase(uvm_phase phase);
-        //extern function void end_of_elaboration_phase(uvm_phase phase);
         extern task run_phase(uvm_phase phase);
 endclass
-//////----------constructor-------///////
+
 function test_1::new(string name = "test_1", uvm_component parent);
         super.new(name, parent);
 endfunction
+
 
 ////---------build_phase----------///////
 function void test_1::build_phase(uvm_phase phase);
         super.build_phase(phase);
 endfunction
 
-///////------end_of_elaboration_phase----------//////
-//function void test_1::end_of_elaboration_phase(uvm_phase phase);
-//      uvm_top.print_toplogy();
-//endfunction
-
 /////////-------run_phase---------////////
 task test_1::run_phase(uvm_phase phase);
 
         phase.raise_objection(this);
 
-        t_seqh = vseq::type_id::create("t_seqh");
-        t_seqh.start(e_cfg.vseqrh);
+        t_seqh = test_sequence::type_id::create("t_seqh");
+        t_seqh.start(e_cfg.ahb_top.agt[0].ahb_seqr);
 
         phase.drop_objection(this);
 endtask
+
+////---------test 1------//////
+class test_2 extends ahb_apb_bridge_test;
+
+        `uvm_component_utils(test_2)
+
+       // vseq t_seqh;
+        write_back t_seqh;
+
+        extern function new(string name = "test_2", uvm_component parent);
+        extern function void build_phase(uvm_phase phase);
+        extern task run_phase(uvm_phase phase);
+endclass
+
+function test_2::new(string name = "test_2", uvm_component parent);
+        super.new(name, parent);
+endfunction
+
+
+////---------build_phase----------///////
+function void test_2::build_phase(uvm_phase phase);
+        super.build_phase(phase);
+endfunction
+
+/////////-------run_phase---------////////
+task test_2::run_phase(uvm_phase phase);
+
+        phase.raise_objection(this);
+
+        t_seqh = write_back::type_id::create("t_seqh");
+        t_seqh.start(e_cfg.ahb_top.agt[0].ahb_seqr);
+	#20ns;
+        phase.drop_objection(this);
+endtask
+
