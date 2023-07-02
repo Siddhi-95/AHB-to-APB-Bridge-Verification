27e2bfb4216cf35cde6b2581e49112929a2d5e30
Siddhi <100703756+Siddhi-95@users.noreply.github.com>
2023-07-02 20:45:55 +0530

    Update apb_driver.sv

--- APB_agt/apb_driver.sv
+++ APB_agt/apb_driver.sv
@@ -1,57 +1,58 @@
-class apb_driver extends uvm_driver#(apb_xtns);
+class apb_driver extends uvm_driver #(apb_xtn);
 
-        //factory registeration
-        `uvm_component_utils (apb_driver)
+	//factory registration
+	`uvm_component_utils(apb_driver)
 
-        //local handle for interface
-        virtual apb_if.AHP_DRV_MP vif;
-        apb_xtns xtn;
-        apb_config apb_cfg;
+	virtual apb_if.APB_DR_MP vif;
 
-        extern function new(string name = "apb_driver", uvm_component parent);
-        extern function void build_phase(uvm_phase phase);
-        extern function void connect_phase(uvm_phase phase);
-        extern task run_phase(uvm_phase phase);
-        extern task send_to_dut(apb_xtns xtn);
+	apb_xtn xtn;
+
+	apb_agent_config apb_cfg;
+
+	extern function new(string name="APB_DRIVER",uvm_component parent);
+	extern function void build_phase(uvm_phase phase);
+	extern function void connect_phase(uvm_phase phase);
+	extern task run_phase(uvm_phase);
+	extern task send_to_dut(apb_xtn xtn);	
 
 endclass
 
-////////---constructor-----//////
-function apb_driver::new(string name = "apb_driver", uvm_component parent);
-        super.new(name,parent);
+//---------Constructor-----------//
+function apb_driver::new(string name="APB_DRIVER",uvm_component parent);
+	super.new(name,parent);
 endfunction
 
-/////------Build Phase-----/////
+//----------Build Phase---------//
 function void apb_driver::build_phase(uvm_phase phase);
-        super.build_phase(phase);
+	super.build_phase(phase);
 
-        if(!uvm_config_db #(apb_config)::get(this, "", "apb_config", apb_cfg))
-                `uvm_fatal("apb_driver", "cannot get the config file")
-
+	if(!uvm_config_db #(apb_agent_config)::get(this,"","apb_agent_config",apb_cfg))
+		`uvm_fatal("DRIVER","cannot get config data");
+	super.build_phase(phase);
 endfunction
 
-////-----connect phase-------///////
+//---------------Connect Phase-------------//
 function void apb_driver::connect_phase(uvm_phase phase);
-        super.connect_phase(phase);
-        vif = apb_cfg.aif;
+	super.connect_phase(phase);
+	vif=apb_cfg.vif;
 endfunction
 
 //////---Run Phase-----/////
 task apb_driver::run_phase(uvm_phase phase);
 
-        req = apb_xtns::type_id::create("req", this);
+        req = apb_xtn::type_id::create("req", this);
 
         forever
                 begin
-                        seq_item_port.get_next_item(req);
+                        //seq_item_port.get_next_item(req);
                         send_to_dut(req);
-        //              req.print();
-                        seq_item_port.item_done();
+                        //req.print();
+                        //seq_item_port.item_done();
                 end
 endtask
 
 ///----send to dut------/////
-task apb_driver::send_to_dut(apb_xtns xtn);
+task apb_driver::send_to_dut(apb_xtn xtn);
         wait(vif.apb_drv_cb.Pselx != 0); //wait for Psel to be high
 
         if(vif.apb_drv_cb.Pwrite == 0) //the moment Psel is high, check for Pwrite signal
@@ -59,6 +60,6 @@ task apb_driver::send_to_dut(apb_xtns xtn);
 
         repeat(2) @(vif.apb_drv_cb); //wait for 2 cycles b4 generating next xtn
         //`uvm_info("apb_driver", "Displaying apb_driver data", UVM_LOW)
-        //xtn.print();
+ //       xtn.print();
         apb_cfg.drv_data_count++;
 endtask
