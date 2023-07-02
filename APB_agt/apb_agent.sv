6ec86bab8ce0a08cc3066a6a2db1c60c0f8f7aa9
Siddhi <100703756+Siddhi-95@users.noreply.github.com>
2023-07-02 20:44:48 +0530

    Update apb_agent.sv

--- APB_agt/apb_agent.sv
+++ APB_agt/apb_agent.sv
@@ -1,48 +1,45 @@
-/////////-----apb AGENT-------//////-------//////
-
 class apb_agent extends uvm_agent;
+	
+	//factory registration
+	`uvm_component_utils(apb_agent)
 
-        `uvm_component_utils(apb_agent)
+	apb_driver apb_drv;
+	apb_monitor apb_mon;
+	apb_sequencer apb_seqr;
 
-        apb_config apb_cfg;
-        apb_driver apb_drv;
-        apb_monitor apb_mon;
-        apb_sequencer apb_seqr;
+	apb_agent_config apb_cfg;
 
-        extern function new(string name = "apb_agent", uvm_component parent);
-        extern function void build_phase (uvm_phase phase);
-        extern function void connect_phase (uvm_phase phase);
-        extern task run_phase(uvm_phase phase);
+	extern function new(string name = "apb_agent", uvm_component parent);
+	extern function void build_phase(uvm_phase phase);
+	extern function void connect_phase (uvm_phase phase);
 endclass
 
-/////-------constructor-----//////
-function apb_agent::new(string name = "apb_agent", uvm_component parent);
-        super.new(name, parent);
+//-------------Constructor--------//
+function apb_agent::new(string name="apb_agent", uvm_component parent);
+	super.new(name,parent);
 endfunction
 
-////-----Build phase---//////
+//------------Build Phase---------//
 function void apb_agent::build_phase(uvm_phase phase);
-        super.build_phase(phase);
+	super.build_phase(phase);
 
-        if(!uvm_config_db #(apb_config)::get(this, "", "apb_config", apb_cfg))
-                `uvm_fatal("apb_agent", "cannot get the config file")
+	if(!uvm_config_db#(apb_agent_config)::get(this,"","apb_agent_config",apb_cfg))
+		`uvm_fatal(get_type_name,"unable to get configuration")
 
-
-        apb_mon = apb_monitor::type_id::create("apb_mon", this);
-
-        if(apb_cfg.is_active == UVM_ACTIVE)
+        apb_mon = apb_monitor::type_id::create("apb_mon",this);
+        if(apb_cfg.is_active==UVM_ACTIVE)
         begin
-                apb_drv = apb_driver::type_id::create("apb_drv", this);
-				apb_seqr = apb_sequencer::type_id::create("apb_seqr", this);
+        	apb_drv=apb_driver::type_id::create("apb_drv",this);
+                apb_seqr=apb_sequencer::type_id::create("apb_seqr",this);
         end
 endfunction
 
-////------connect_phase-------///////
+//-------------connect phase---------//
 function void apb_agent::connect_phase(uvm_phase phase);
-        super.connect_phase(phase);
-        apb_drv.seq_item_port.connect(apb_seqr.seq_item_export);
-endfunction
+	super.connect_phase(phase);
 
-task apb_agent::run_phase(uvm_phase phase);
-                uvm_top.print_topology;
-endtask
+        if(apb_cfg.is_active==UVM_ACTIVE)
+        begin
+        	apb_drv.seq_item_port.connect(apb_seqr.seq_item_export);
+        end
+endfunction
