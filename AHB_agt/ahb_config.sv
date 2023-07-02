723d03b7225311b0312c590877ec1d4da8e8a73b
Siddhi <100703756+Siddhi-95@users.noreply.github.com>
2023-07-02 20:36:00 +0530

    Update ahb_config.sv

--- AHB_agt/ahb_config.sv
+++ AHB_agt/ahb_config.sv
@@ -1,18 +1,15 @@
-/////////AHB CONFIG////////
-
-class ahb_config extends uvm_object;
-
-        `uvm_object_utils (ahb_config)
-        virtual ahb_if aif;
-        uvm_active_passive_enum is_active = UVM_ACTIVE;
-
-        static int drv_data_count = 0;
+class ahb_agent_config extends uvm_object;
+	//factory registration
+	`uvm_object_utils (ahb_agent_config)
+	virtual ahb_if vif;
+	uvm_active_passive_enum is_active;
+	
+	static int drv_data_count = 0;
         static int mon_data_count = 0;
 
-        extern function new(string name = "ahb_config");
+	extern function new (string name = "ahb_agent_config");
 endclass
 
-//---constructor-----////
-function ahb_config::new(string name = "ahb_config");
-        super.new(name);
+function ahb_agent_config::new(string name = "ahb_agent_config");
+	super.new(name);
 endfunction
