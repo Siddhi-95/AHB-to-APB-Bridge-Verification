399f4849dc88364fef90e8477c0289f92ccbfb64
Siddhi <100703756+Siddhi-95@users.noreply.github.com>
2023-07-02 21:02:29 +0530

    Update APB_AHB_pkg.sv

--- test/APB_AHB_pkg.sv
+++ test/APB_AHB_pkg.sv
@@ -1,11 +1,12 @@
-package APB_AHB_pkg;
+package ahb_apb_bridge_pkg;
 
         import uvm_pkg::*;
         `include "uvm_macros.svh"
-        `include "ahb_xtns.sv"
-        `include "ahb_config.sv"
-        `include "apb_config.sv"
-        `include "AHB_APB_env_config.sv"
+//`include "tb_defs.sv"
+        `include "ahb_xtn.sv"
+        `include "apb_agent_config.sv"
+        `include "ahb_agent_config.sv"
+        `include "ahb_apb_env_config.sv"
         `include "ahb_driver.sv"
         `include "ahb_monitor.sv"
         `include "ahb_sequencer.sv"
@@ -13,19 +14,19 @@ package APB_AHB_pkg;
         `include "ahb_agt_top.sv"
         `include "ahb_seqs.sv"
 
-        `include "apb_xtns.sv"
-        `include "apb_driver.sv"
+        `include "apb_xtn.sv"
+	`include "apb_driver.sv"
         `include "apb_monitor.sv"
         `include "apb_sequencer.sv"
         `include "apb_agent.sv"
         `include "apb_agt_top.sv"
-        `include "apb_seqs.sv"
+	`include "apb_seqs.sv"
 
-        `include "virtual_sequencer.sv"
-        `include "virtual_sequence.sv"
         `include "scoreboard.sv"
 
-        `include "AHB_APB_env.sv"
-        `include "test.sv"
+   //     `include "tb.sv"
 
+        `include "ahb_apb_env.sv"
+        `include "test.sv"
 endpackage
+
