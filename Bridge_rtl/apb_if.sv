65773020df96d582b113d1326da3ac34d2f6b7ca
Siddhi <100703756+Siddhi-95@users.noreply.github.com>
2023-07-02 20:59:53 +0530

    Update apb_if.sv

--- Bridge_rtl/apb_if.sv
+++ Bridge_rtl/apb_if.sv
@@ -14,13 +14,36 @@ interface apb_if (input bit clock);
         //APB select - master
         logic [3:0] Pselx;
 
+
+property only_one_bit_high_Psel;
+        @(posedge clock)  (Pselx == 4'b0000 || Pselx == 4'b1000 || Pselx == 4'b0100 || Pselx == 4'b0010 || Pselx == 4'b0001);
+            
+ //       @(posedge clock) $onehot0(Pselx);
+
+endproperty;
+
+ONLY_ONE_BIT_HIGH_PSEL: assert property (only_one_bit_high_Psel);
+       
+property penable_high_for_one_cycle;
+	@(posedge clock) Penable[->1] |=> !(Penable);
+endproperty
+
+      PENABLE_HIGH: assert property (penable_high_for_one_cycle);
+
+property after_addr_change_penable_high;
+	@(posedge clock) 1'b1 ##1 $changed(Paddr) |=> Penable[->1] ##1 !(Penable);
+endproperty
+
+     ADDR_CHANGE_PENABLE_HIGH: assert property (after_addr_change_penable_high);
+
+
         //APB Driver
         clocking apb_drv_cb @(posedge clock);
                 default input #1 output #1;
                 output Prdata;
                 input Penable;
                 input Pwrite;
-                input Pselx; //doubt
+                input Pselx; 
         endclocking
 
         //APB monitor
@@ -34,7 +57,7 @@ interface apb_if (input bit clock);
                 input Pwdata;
         endclocking
 	//DRIVER and monitor modport:
-    modport APB_DRV_MP (clocking apb_drv_cb);
+    modport APB_DR_MP (clocking apb_drv_cb);
     modport APB_MON_MP (clocking apb_mon_cb);
 
 endinterface: apb_if
