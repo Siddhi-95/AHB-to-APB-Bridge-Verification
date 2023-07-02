5d699b379c0ee325acf538f7fd7830a82e825a1e
Siddhi <100703756+Siddhi-95@users.noreply.github.com>
2023-07-02 20:45:35 +0530

    Update apb_config.sv

--- APB_agt/apb_config.sv
+++ APB_agt/apb_config.sv
@@ -1,21 +1,18 @@
-/////////apb CONFIG////////
+class apb_agent_config extends uvm_object;
 
-class apb_config extends uvm_object;
+	// UVM Factory Registration Macro
+	`uvm_object_utils(apb_agent_config)
+	virtual apb_if vif;
+	uvm_active_passive_enum is_active;
 
-        `uvm_object_utils (apb_config)
-        virtual apb_if aif;
-
-        //data count variables:
-        static int drv_data_count = 0;
+	static int drv_data_count = 0;
         static int mon_data_count = 0;
 
+	extern function new(string name = "apb_agent_config");
 
-        uvm_active_passive_enum is_active = UVM_ACTIVE;
-
-        extern function new(string name = "apb_config");
 endclass
 
-//---constructor-----////
-function apb_config::new(string name = "apb_config");
-        super.new(name);
+function apb_agent_config::new(string name = "apb_agent_config");
+	super.new(name);
 endfunction
+
