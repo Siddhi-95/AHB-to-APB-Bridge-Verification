953f45c4eac9a34d4fc8cdb510729bac1de09d2e
Siddhi <100703756+Siddhi-95@users.noreply.github.com>
2023-07-02 21:00:55 +0530

    Update scoreboard.sv

--- tb/scoreboard.sv
+++ tb/scoreboard.sv
@@ -1,202 +1,217 @@
+//Scoreboard
+
 class scoreboard extends uvm_scoreboard;
+	
+	//factory registration
+	`uvm_component_utils(scoreboard)
+	
+	//Analysis FIFOs declaration
+	uvm_tlm_analysis_fifo #(ahb_xtn) fifo_ahb[];
+	uvm_tlm_analysis_fifo #(apb_xtn) fifo_apb[];
+	
+	//declare the handles for ahb_xtns and apb_xtns
+	ahb_xtn ahb_data;
+	apb_xtn apb_data;
+	
+	//handles for read and write coverage data
+	ahb_xtn ahb_cov_data;
+	apb_xtn apb_cov_data;
 
-        `uvm_component_utils (scoreboard)
+	//environment configuration object(handle) declaration:
+	ahb_apb_env_config e_cfg;
+	
+	//define a queue to push data of AHB to compare it with APB
+	ahb_xtn q[$];	
+	//apb_xtn q1[$];
 
-        uvm_tlm_analysis_fifo #(ahb_xtns) ahb_fifo[];
-        uvm_tlm_analysis_fifo #(apb_xtns) apb_fifo[];
+	int data_verified_count;
 
-        //declare handles for ahb and apb txn class
-        ahb_xtns ahb_xtn;
-        apb_xtns check_xtn;
+	//Std UVM Methods
+	extern function new (string name = "scoreboard", uvm_component parent);
+	extern function void build_phase (uvm_phase phase);
+	extern task run_phase (uvm_phase phase);
+	extern function void check_data (apb_xtn xtn);//user-defined function
+	extern function void compare_data (int Hdata, Pdata, Haddr, Paddr);
 
-        //Declare the handles for read & write coverage data of type read_xtn and write_xtn respectively
-        ahb_xtns ahb_cov_data;
-        apb_xtns apb_cov_data;
+	//coverage for AHB
+	covergroup ahb_cg;
+		option.per_instance = 1;
 
-        AHB_APB_env_config m_cfg;
+		//RST: coverpoint ahb_cov_data.Hresetn;
+		
+		SIZE: coverpoint ahb_cov_data.Hsize {bins b2[] = {[0:2]} ;}//1,2,4 bytes of data
+		
+		TRANS: coverpoint ahb_cov_data.Htrans {bins trans[] = {[2:3]} ;}//NS and S
+		
+		//BURST: coverpoint ahb_cov_data.Hburst {bins burst[] = {[0:7]} ;}
+		
+		ADDR: coverpoint ahb_cov_data.Haddr {bins first_slave = {[32'h8000_0000:32'h8000_03ff]} ;
+						     bins second_slave = {[32'h8400_0000:32'h8400_03ff]};
+                                                     bins third_slave = {[32'h8800_0000:32'h8800_03ff]};
+                                                     bins fourth_slave = {[32'h8C00_0000:32'h8C00_03ff]};}
 
-        //define a queue to push data of AHB to compare it with APB
-        ahb_xtns q[$];
-
-        int data_verified_count;
-
-        extern function new(string name="scoreboard", uvm_component parent);
-        extern function void build_phase(uvm_phase phase);
-        extern task run_phase(uvm_phase phase);
-        extern function void check_data(apb_xtns xtn); //user-defined function
-        extern function void compare_data(int Hdata, Pdata, Haddr, Paddr);
-
-        //coverage for AHB
-        covergroup ahb_cg;
-                option.per_instance = 1;
-
-                //RST : coverpoint ahb_cov_data.Hresetn {bins low = {[0:1]} ;}
-                SIZE : coverpoint ahb_cov_data.Hsize {bins ad1[] = {[0:2]};} //1/2/4 bytes of data
-				TRANS: coverpoint ahb_cov_data.Htrans {bins trans[] = {[2:3]};} //NS and Sequential
-                //Burst : coverpoint ahb_cov_data.Hburst {bins burst = {[0:7};}
-                ADDR : coverpoint ahb_cov_data.Haddr {bins first_slave = {[32'h8000_0000:32'h8000_03ff]};
-                                                                                          bins second_slave = {[32'h8400_0000:32'h8400_03ff]};
-                                                                                          bins third_slave = {[32'h8800_0000:32'h8800_03ff]};
-                                                                                          bins fourth_slave = {[32'h8C00_0000:32'h8C00_03ff]};}
-
-                DATA_IN : coverpoint ahb_cov_data.Hwdata {bins low = {[0:32'h0000_ffff]};
-                                                                                                  bins mid1 = {[32'h0001_ffff:32'hffff_ffff]};}
+		DATA_IN: coverpoint ahb_cov_data.Hwdata {bins low = {[0:32'h0000_ffff]};
+                                                         bins mid1 = {[32'h0001_ffff:32'hffff_ffff]};}
 
                 DATA_OUT : coverpoint ahb_cov_data.Hrdata {bins low = {[0:32'h0000_ffff]};
-                                                                                                  bins mid1 = {[32'h0001_ffff:32'hffff_ffff]};}
+                                                           bins mid1 = {[32'h0001_ffff:32'hffff_ffff]};}
+		WRITE : coverpoint ahb_cov_data.Hwrite;
 
-                //resp : coverpoint ahb_cov_data.Hresp {bins resp[] = {[0,1]};}
+		SIZEXWRITE: cross SIZE, WRITE;
 
-                WRITE : coverpoint ahb_cov_data.Hwrite;
-                //ready : coverpoint ahb_cov_data.Hreadyout;
-        endgroup: ahb_cg
+		//ADDRXDATA: cross ahb_cov_data.Haddr, ahb_cov_data.Hwdata;
+	endgroup: ahb_cg
 
-        covergroup apb_cg;
-                option.per_instance = 1;//its not options, simply option
+	covergroup apb_cg;
+		option.per_instance = 1;
+		
+		ADDR : coverpoint apb_cov_data.Paddr {bins first_slave = {[32'h8000_0000:32'h8000_03ff]};
+                                                      bins second_slave = {[32'h8400_0000:32'h8400_03ff]};
+                                                      bins third_slave = {[32'h8800_0000:32'h8800_03ff]};
+                                                      bins fourth_slave = {[32'h8C00_0000:32'h8C00_03ff]};}
+				
+		DATA_IN : coverpoint apb_cov_data.Pwdata {bins low = {[0:32'h0000_ffff]};
+                                                          bins mid1 = {[32'h0001_ffff:32'hffff_ffff]};}
 
-                ADDR : coverpoint apb_cov_data.Paddr {bins first_slave = {[32'h8000_0000:32'h8000_03ff]};
-                                                                                          bins second_slave = {[32'h8400_0000:32'h8400_03ff]};
-                                                                                          bins third_slave = {[32'h8800_0000:32'h8800_03ff]};
-                                                                                          bins fourth_slave = {[32'h8C00_0000:32'h8C00_03ff]};}
-				DATA_IN : coverpoint apb_cov_data.Pwdata {bins low = {[0:32'h0000_ffff]};
-                                                                                                  bins mid1 = {[32'h0001_ffff:32'hffff_ffff]};}
-
-                DATA_OUT : coverpoint apb_cov_data.Prdata {bins low = {[0:32'h0000_ffff]};
-                                                                                                  bins mid1 = {[32'h0001_ffff:32'hffff_ffff]};}
+                DATA_OUT : coverpoint apb_cov_data.Prdata {bins low = {[0:32'hffff_ffff]};}
 
                 WRITE : coverpoint apb_cov_data.Pwrite;
 
                 SEL : coverpoint apb_cov_data.Pselx {bins first_slave = {4'b0001};
-                                                                                         bins second_slave = {4'b0010};
-                                                                                         bins third_slave = {4'b0100};
-                                                                                         bins fourth_slave = {4'b1000};}
-        endgroup: apb_cg
+                                                     bins second_slave = {4'b0010};
+                                                     bins third_slave = {4'b0100};
+                                                     bins fourth_slave = {4'b1000};}
 
+		WRITEXSEL: cross WRITE, SEL;
+		//ADDRXWRITE: cross apv_cov_data.Paddr, apb_cov_data.Pwrite;
+	endgroup: apb_cg
 endclass: scoreboard
 
-//-----Constructor------//
-function scoreboard::new(string name = "scoreboard", uvm_component parent);
-        super.new(name, parent);
-        ahb_cov_data = new;
-        apb_cov_data = new;
-        ahb_cg = new();
+function scoreboard::new (string name = "scoreboard", uvm_component parent);
+	super.new (name, parent);
+	ahb_cov_data = new();
+	apb_cov_data = new();
+	ahb_cg = new();
         apb_cg = new();
 endfunction: new
 
-//-------build phase-------//
-function void scoreboard::build_phase(uvm_phase phase);
-        super.build_phase(phase);
-
-        if(!uvm_config_db #(AHB_APB_env_config)::get(this,"","AHB_APB_env_config",m_cfg))
-			`uvm_fatal("CONFIG","Cannot get() e_cfg, have you set() it?")
-
-        ahb_fifo = new[m_cfg.no_of_ahb_agents];
-        apb_fifo = new[m_cfg.no_of_apb_agents];
-
-        foreach(ahb_fifo[i])
-        begin
-                ahb_fifo[i] = new($sformatf("ahb_fifo[%0d]",i),this);
-        end
-
-        foreach(apb_fifo[i])
-        begin
-                apb_fifo[i] = new($sformatf("apb_fifo[%0d]",i),this);
-        end
-
+function void  scoreboard::build_phase(uvm_phase phase);
+	super.build_phase(phase);
+	
+	//get the config object e_cfg
+	if(!uvm_config_db #(ahb_apb_env_config)::get(this, "", "ahb_apb_env_config", e_cfg))
+		`uvm_fatal("CONFIG", "Cannot get() e_cfg, have you set() it?")
+		
+	//create object for analysis fifo
+	fifo_ahb = new[e_cfg.no_of_ahb_agents];
+	fifo_apb = new[e_cfg.no_of_apb_agents];
+	
+	foreach(fifo_ahb[i])
+	begin
+		fifo_ahb[i] = new($sformatf("fifo_ahb[%0d]",i), this);
+	end
+	
+	foreach(fifo_apb[i])
+	begin
+		fifo_apb[i] = new($sformatf("fifo_apb[%0d]",i), this);
+	end
+	
 endfunction: build_phase
 
 task scoreboard::run_phase(uvm_phase phase);
 
-        fork
-                begin
-                forever
-                        begin
-                                ahb_fifo[0].get(ahb_xtn);
-                        //      ahb_xtn.display("Data from SB");
-                                q.push_back(ahb_xtn);
-                                $display("Size of the Queue = %d", q.size);
-                                ahb_cov_data = ahb_xtn;
-                                //sample the AHB cover group
-                                ahb_cg.sample();
-                        end
-                end
-				begin
-				forever
-                        begin
-                                apb_fifo[0].get(check_xtn);
-                        //      apb_xtn.display("Data from SB");
-                                check_data(check_xtn);
-                                apb_cov_data = check_xtn;
-                                //sample the APB cover group
-                                apb_cg.sample();
-                        end
-                end
-        join
+	fork
+		
+		begin
+		forever
+			begin
+				fifo_ahb[0].get(ahb_data);
+				q.push_back(ahb_data);
+				$display("Size of the queue = %d", q.size);
+				ahb_cov_data = ahb_data;
+				//sample the AHB CG
+				ahb_cg.sample();
+			end
+		end
 
-endtask:run_phase
+		begin
+		forever
+			begin		
+				fifo_apb[0].get(apb_data);
+				//q1.push_back(apb_data);
+				check_data(apb_data);
+				apb_cov_data = apb_data;
+				//sample the APB CG
+				apb_cg.sample();
+			end
+		end
+
+
+	join
+endtask: run_phase
 
 //-------comparing AHB and APB data------//
-function void scoreboard::check_data(apb_xtns xtn);
+function void scoreboard::check_data(apb_xtn xtn);
         //ahb transaction data
-        ahb_xtn = q.pop_front();
-
-        if(ahb_xtn.Hwrite)
+        ahb_data = q.pop_front();
+	
+        if(ahb_data.Hwrite)
         begin
-                case(ahb_xtn.Hsize)
+                case(ahb_data.Hsize)
 
                 2'b00:
                 begin
-                        if(ahb_xtn.Haddr[1:0] == 2'b00)
-                                compare_data(ahb_xtn.Hwdata[7:0], check_xtn.Pwdata[7:0], ahb_xtn.Haddr, check_xtn.Paddr);
-                        if(ahb_xtn.Haddr[1:0] == 2'b01)
-                                compare_data(ahb_xtn.Hwdata[15:8], check_xtn.Pwdata[7:0], ahb_xtn.Haddr, check_xtn.Paddr);
-                        if(ahb_xtn.Haddr[1:0] == 2'b10)
-                                compare_data(ahb_xtn.Hwdata[23:16], check_xtn.Pwdata[7:0], ahb_xtn.Haddr, check_xtn.Paddr);
-                        if(ahb_xtn.Haddr[1:0] == 2'b11)
-                                compare_data(ahb_xtn.Hwdata[31:24], check_xtn.Pwdata[7:0], ahb_xtn.Haddr, check_xtn.Paddr);
+                        if(ahb_data.Haddr[1:0] == 2'b00)
+                                compare_data(ahb_data.Hwdata[7:0], apb_data.Pwdata[7:0], ahb_data.Haddr, apb_data.Paddr);
+                        if(ahb_data.Haddr[1:0] == 2'b01)
+                                compare_data(ahb_data.Hwdata[15:8], apb_data.Pwdata[7:0], ahb_data.Haddr, apb_data.Paddr);
+                        if(ahb_data.Haddr[1:0] == 2'b10)
+                                compare_data(ahb_data.Hwdata[23:16], apb_data.Pwdata[7:0], ahb_data.Haddr, apb_data.Paddr);
+                        if(ahb_data.Haddr[1:0] == 2'b11)
+                                compare_data(ahb_data.Hwdata[31:24], apb_data.Pwdata[7:0], ahb_data.Haddr, apb_data.Paddr);
 
                 end
-				2'b01:
+		2'b01:
                 begin
-                        if(ahb_xtn.Haddr[1:0] == 2'b00)
-                                compare_data(ahb_xtn.Hwdata[15:0], check_xtn.Pwdata[15:0], ahb_xtn.Haddr, check_xtn.Paddr);
-                        if(ahb_xtn.Haddr[1:0] == 2'b10)
-                                compare_data(ahb_xtn.Hwdata[31:16], check_xtn.Pwdata[15:0], ahb_xtn.Haddr, check_xtn.Paddr);
+                        if(ahb_data.Haddr[1:0] == 2'b00)
+                                compare_data(ahb_data.Hwdata[15:0], apb_data.Pwdata[15:0], ahb_data.Haddr, apb_data.Paddr);
+                        if(ahb_data.Haddr[1:0] == 2'b10)
+                                compare_data(ahb_data.Hwdata[31:16], apb_data.Pwdata[15:0], ahb_data.Haddr, apb_data.Paddr);
                 end
 
                 2'b10:
-                        compare_data(ahb_xtn.Hwdata, check_xtn.Pwdata, ahb_xtn.Haddr, check_xtn.Paddr);
+                        compare_data(ahb_data.Hwdata, apb_data.Pwdata, ahb_data.Haddr, apb_data.Paddr);
 
                 endcase
         end
 
         else
         begin
-                case(ahb_xtn.Hsize)
+                case(ahb_data.Hsize)
 
                 2'b00:
                 begin
-                        if(ahb_xtn.Haddr[1:0] == 2'b00)
-                                compare_data(ahb_xtn.Hrdata[7:0], check_xtn.Prdata[7:0], ahb_xtn.Haddr, check_xtn.Paddr);
-                        if(ahb_xtn.Haddr[1:0] == 2'b01)
-                                compare_data(ahb_xtn.Hrdata[15:8], check_xtn.Prdata[7:0], ahb_xtn.Haddr, check_xtn.Paddr);
-                        if(ahb_xtn.Haddr[1:0] == 2'b10)
-                                compare_data(ahb_xtn.Hrdata[31:16], check_xtn.Prdata[7:0], ahb_xtn.Haddr, check_xtn.Paddr);
-                        if(ahb_xtn.Haddr[1:0] == 2'b11)
-                                compare_data(ahb_xtn.Hrdata[31:16], check_xtn.Prdata[7:0], ahb_xtn.Haddr, check_xtn.Paddr);
+                        if(ahb_data.Haddr[1:0] == 2'b00)
+                                compare_data(ahb_data.Hrdata[7:0], apb_data.Prdata[7:0], ahb_data.Haddr, apb_data.Paddr);
+                        if(ahb_data.Haddr[1:0] == 2'b01)
+                                compare_data(ahb_data.Hrdata[7:0], apb_data.Prdata[15:8], ahb_data.Haddr, apb_data.Paddr);
+                        if(ahb_data.Haddr[1:0] == 2'b10)
+                                compare_data(ahb_data.Hrdata[7:0], apb_data.Prdata[23:16], ahb_data.Haddr, apb_data.Paddr);
+                        if(ahb_data.Haddr[1:0] == 2'b11)
+                                compare_data(ahb_data.Hrdata[7:0], apb_data.Prdata[31:24], ahb_data.Haddr, apb_data.Paddr);
 
                 end
 
                 2'b01:
                 begin
-						if(ahb_xtn.Haddr[1:0] == 2'b00)
-                                compare_data(ahb_xtn.Hrdata[15:0], check_xtn.Prdata[15:0], ahb_xtn.Haddr, check_xtn.Paddr);
-						if(ahb_xtn.Haddr[1:0] == 2'b10)
-                                compare_data(ahb_xtn.Hrdata[31:16], check_xtn.Prdata[15:0], ahb_xtn.Haddr, check_xtn.Paddr);
+			if(ahb_data.Haddr[1:0] == 2'b00)
+                                compare_data(ahb_data.Hrdata[15:0], apb_data.Prdata[15:0], ahb_data.Haddr, apb_data.Paddr);
+			if(ahb_data.Haddr[1:0] == 2'b10)
+                                compare_data(ahb_data.Hrdata[15:0], apb_data.Prdata[31:16], ahb_data.Haddr, apb_data.Paddr);
                 end
 
                 2'b10:
-                        compare_data(ahb_xtn.Hrdata, check_xtn.Prdata, ahb_xtn.Haddr, check_xtn.Paddr);
+                        compare_data(ahb_data.Hrdata, apb_data.Prdata, ahb_data.Haddr, apb_data.Paddr);
 
                 endcase
         end
@@ -205,24 +220,30 @@ endfunction
 function void scoreboard::compare_data(int Hdata, Pdata, Haddr, Paddr);
 
         if(Haddr == Paddr)
+	begin
                 $display("Address compared Successfully");
+		$display("HADDR=%h, PADDR=%h", Haddr, Paddr);
+	end
         else
         begin
                 $display("Address not compared Successfully");
-                $finish;
+		$display("HADDR=%h, PADDR=%h", Haddr, Paddr);
+               // $finish;
         end
 
-        if(Hdata == Pdata)
-                $display("Address compared Successfully");
+        if(Hdata == Pdata) 
+	begin
+                $display("Data compared Successfully");
+		$display("HDATA=%h, PDATA=%h", Hdata, Pdata);
+	end
         else
         begin
-                $display("Address not compared Successfully");
-                $finish;
+                $display("Data not compared Successfully");
+		$display("HDATA=%h, PDATA=%h", Hdata, Pdata);
+               // $finish;
         end
 
         data_verified_count ++;
+	$display ("Data verified = %d", data_verified_count);
 endfunction
-
-                         
-
 
