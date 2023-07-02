e7ac0028a2bbc70e67a618c617d98944e1f77fc1
Siddhi <100703756+Siddhi-95@users.noreply.github.com>
2023-07-02 20:45:14 +0530

    Update apb_agt_top.sv

--- APB_agt/apb_agt_top.sv
+++ APB_agt/apb_agt_top.sv
@@ -1,37 +1,34 @@
 class apb_agt_top extends uvm_env;
+	
+	//factory reg
+	`uvm_component_utils(apb_agt_top)
 
-        //factory registration
-        `uvm_component_utils(apb_agt_top)
+	ahb_apb_env_config m_cfg;
+	apb_agent agt[];
 
-        //apb agent
-        apb_agent apb_agth[];
-
-        //env_config object
-        AHB_APB_env_config m_cfg;
-
-        extern function new(string name = "apb_agt_top", uvm_component parent);
+	//uvm_methods
+        extern function new(string name = "apb_agt_top" , uvm_component parent);
         extern function void build_phase(uvm_phase phase);
 
-endclass: apb_agt_top
+endclass
 
-/////////--------constructor-------/////////
-function apb_agt_top::new(string name = "apb_agt_top", uvm_component parent);
-        super.new(name, parent);
-endfunction: new
+//code for new
+function apb_agt_top::new(string name="apb_agt_top",uvm_component parent);
+	super.new(name,parent);
+endfunction
 
-///////-------Build phase-------///////////
-function apb_agt_top::build_phase(uvm_phase phase);
-        super.build_phase(phase);
+//code for build phase
+function void apb_agt_top::build_phase(uvm_phase phase);
+	
+	super.build_phase(phase);
+	if(!uvm_config_db #(ahb_apb_env_config)::get(this,"","ahb_apb_env_config",m_cfg))
+		`uvm_fatal(get_type_name,"not able to get the configuration")
 
-        if(!uvm_config_db #(AHB_APB_env_config)::get(this,"","AHB_APB_env_config",m_cfg))
-                `uvm_fatal(get_type_name(),"ENV: read error")
+	agt =new[m_cfg.no_of_apb_agents];
+	foreach(agt[i])
+	begin
+		agt[i]=apb_agent::type_id::create($sformatf("agt[%0d]", i),this);
+		uvm_config_db #(apb_agent_config)::set(this,$sformatf("agt[%0d]*", i), "apb_agent_config", m_cfg.apb_cfg[i]);
+	end
 
-                apb_agth = new[m_cfg.no_of_apb_agents];
-        foreach(apb_agth[i])
-                begin
-                        apb_agth[i]=apb_agent::type_id::create($sformatf("apb_agth[%0d]",i), this);
-                        uvm_config_db #(apb_config)::set(this,$sformatf("apb_agth[%0d]*", i),"apb_config", m_cfg.apb_config[i]);
-                end
-
-endfunction : build_phase
-                             
+endfunction
