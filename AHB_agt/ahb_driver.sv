612b365ea3f2a19d1574b72a69841b59de2f160d
Siddhi <100703756+Siddhi-95@users.noreply.github.com>
2023-07-02 20:36:27 +0530

    Update ahb_driver.sv

--- AHB_agt/ahb_driver.sv
+++ AHB_agt/ahb_driver.sv
@@ -1,42 +1,47 @@
-///AHB Driver//////
+//AHB_Driver
 
-class ahb_driver extends uvm_driver#(ahb_xtns);
+class ahb_driver extends uvm_driver #(ahb_xtn);
+	
+	//factory registration
+	`uvm_component_utils (ahb_driver)
+	
+	//interface
+	virtual ahb_if.AHB_DR_MP vif;
+	
+	ahb_xtn xtn;
 
-        //factory registeration
-        `uvm_component_utils (ahb_driver)
+	ahb_agent_config ahb_cfg;
+	
+	extern function new (string name = "ahb_driver", uvm_component parent);
+	extern function void build_phase (uvm_phase phase);
+	extern function void connect_phase (uvm_phase phase);
+	extern task run_phase(uvm_phase phase);
+	extern task send_to_dut (ahb_xtn xtn);
+endclass: ahb_driver
 
-        //local handle for interface
-        virtual ahb_if.AHB_DRV_MP vif;
+//-----------Constructor----------//
+function ahb_driver::new (string name = "ahb_driver", uvm_component parent);
+	super.new (name, parent);
+endfunction: new
 
-        ahb_xtns xtn;
-        ahb_config ahb_cfg;
+//-----------Build Phase---------//
+function void ahb_driver::build_phase (uvm_phase phase);
+	super.build_phase(phase);
+	
+	if(!uvm_config_db #(ahb_agent_config)::get(this, "", "ahb_agent_config", ahb_cfg))
+	//m_cfg is the local handle through which we want to read the variable from config db
+	//first 3 parameters of get method gives the address 
+		`uvm_fatal("CONFIG","Cannot get() m_cfg from uvm_config_db. Have you set it?")
+endfunction: build_phase
 
-        extern function new(string name = "ahb_driver", uvm_component parent);
-        extern function void build_phase(uvm_phase phase);
-        extern function void connect_phase(uvm_phase phase);
-        extern task run_phase(uvm_phase phase);
-        extern task send_to_dut(ahb_xtns xtn);
-
-endclass
-
-////////---constructor-----//////
-function ahb_driver::new(string name = "ahb_driver", uvm_component parent);
-        super.new(name,parent);
-endfunction
-
-/////------Build Phase-----/////
-function void ahb_driver::build_phase(uvm_phase phase);
-        super.build_phase(phase);
-
-        if(!uvm_config_db #(ahb_config)::get(this, "", "ahb_config", ahb_cfg))
-                `uvm_fatal("ahb_driver", "cannot get the config file")
-
-endfunction
-////-----connect phase-------///////
-function void ahb_driver::connect_phase(uvm_phase phase);
-        super.connect_phase(phase);
-        vif = ahb_cfg.aif;
-endfunction
+//----------Connect Phase---------//
+function void ahb_driver::connect_phase (uvm_phase phase);
+	super.connect_phase(phase);
+	
+	//interface connection
+	vif=ahb_cfg.vif;
+	
+endfunction: connect_phase
 
 //////---Run Phase-----/////
 task ahb_driver::run_phase(uvm_phase phase);
@@ -45,14 +50,14 @@ task ahb_driver::run_phase(uvm_phase phase);
         @(vif.ahb_drv_cb);
         vif.ahb_drv_cb.Hresetn <= 1'b0;
 
-        repeat(2)
+        repeat(3)
         @(vif.ahb_drv_cb);
         vif.ahb_drv_cb.Hresetn <= 1'b1;
 
         forever
                 begin
-                        seq_item_port.get_next_item(req);
-        //              req.print();
+                        seq_item_port.get_next_item(req); //get the data from sequencer
+                        req.print();
                         send_to_dut(req);
                         seq_item_port.item_done();
                 end
@@ -60,15 +65,15 @@ endtask
 
 
 ///----send to dut------/////
-task ahb_driver::send_to_dut(ahb_xtns xtn);
+task ahb_driver::send_to_dut(ahb_xtn xtn);
 
         //drive addr and control info
         vif.ahb_drv_cb.Hwrite  <= xtn.Hwrite;
         vif.ahb_drv_cb.Htrans <= xtn.Htrans;
         vif.ahb_drv_cb.Hsize   <= xtn.Hsize;
         vif.ahb_drv_cb.Haddr   <= xtn.Haddr;
-        vif.ahb_drv_cb.Hreadyin <= 1'b1; //since we have 1 AHB Master, Hreadyin will always be high indicating to slave that Master is ready to send
-
+        vif.ahb_drv_cb.Hreadyin<= 1'b1;
+		
         @(vif.ahb_drv_cb);
 
         //wait till Hreadyout goes high - the moment it goes high drive Hwdata
@@ -81,5 +86,8 @@ task ahb_driver::send_to_dut(ahb_xtns xtn);
         //After driving Hwdata, we should immediately drive the address in the same cycle, so endtask w/o any delay
 
         ahb_cfg.drv_data_count++;
+	$display("No of packets driven = %0d", ahb_cfg.drv_data_count);
 endtask
 
+/*In AHB_DRIVER, we will declare the local interface handle and assign it to the interface defined in AHB_CONFIG in the connect phase.
+First, get the data from the Sequencer and drive this data to the DUT (Bridge) via the Interface.*/
